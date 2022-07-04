77c4222a508e1a00ec78910dee19ea149af35890
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-07-04 12:14:04 +0100

    new updated

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
-	await app.listen(9080);
+	await app.listen(process.env.PORT || 9080);
 }
 bootstrap();
